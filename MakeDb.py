86fd7a1c2e8d2a41ea5941e58150803bab7cfb8d
LittleSaltFish <saltfish@whut.edu.cn>
2022-01-13 11:20:10 +0800

    补充 MakeDB 脚本，为前端搭建做准备

--- MakeDb.py
+++ MakeDb.py
@@ -2,6 +2,6 @@
 import pandas
 import csv, sqlite3
 conn= sqlite3.connect("./Result/Data.db")
-df = pandas.read_csv('./Result/Data_Step2.csv')
+df = pandas.read_csv('./Result/Data_Step2(WithHeader).csv')
 df.to_sql('Data', conn, if_exists='append', index=False)
 print('ok')
